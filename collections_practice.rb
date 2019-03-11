7bd40f92a8df611fc1a7d2f76951ecc5fc978e47
kyle smith <kylemclellan@gmail.com>
2019-03-11 16:57:30 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,3 +9,10 @@ end
 def sort_array_char_count(array)
   array.sort { |a, b| a.length <=> b.length }
 end
+
+def swap_elements(array)
+  temp = array[1]
+  array[1] = array[2]
+  array[2] = temp
+  array
+end
